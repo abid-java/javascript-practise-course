849f140406f69a904e491eac5271f72d22f49fa8
Akhil Mohammad <Abid Mohammad>
2022-01-05 23:22:35 -0500

    adding logs

--- Basic-01- Starting Project/assets/scripts/vendor.js
+++ Basic-01- Starting Project/assets/scripts/vendor.js
@@ -2,14 +2,16 @@ const userInput = document.getElementById('input-number');
 const addBtn = document.getElementById('btn-addition');
 const subBtn = document.getElementById('btn-subtraction');
 const mulBtn = document.getElementById('btn-multiplication');
-const divBtn = document.getElementById('btn-divison');
+const divBtn = document.getElementById('btn-divide');
 
 const currentResult = document.getElementById('initial-result');
 const calculationResult = document.getElementById('current-calculation');
 
 function outputResult(result, text) {
+    console.log('<===== Testing - 1 =====>')
     currentResult.textContent = result;
     calculationResult.text = text;
+    console.log('<===== Testing - 2 =====>')
 }
 
 
