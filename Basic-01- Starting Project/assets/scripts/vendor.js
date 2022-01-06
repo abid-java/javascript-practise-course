c80c25d116b5619b704dc18e087857eb26d7678b
Akhil Mohammad <Abid Mohammad>
2022-01-05 23:35:27 -0500

    error fixes

--- Basic-01- Starting Project/assets/scripts/vendor.js
+++ Basic-01- Starting Project/assets/scripts/vendor.js
@@ -4,13 +4,13 @@ const subBtn = document.getElementById('btn-subtraction');
 const mulBtn = document.getElementById('btn-multiplication');
 const divBtn = document.getElementById('btn-divide');
 
-const currentResult = document.getElementById('initial-result');
+const calculatedResult = document.getElementById('calculated-result');
 const calculationResult = document.getElementById('current-calculation');
 
 function outputResult(result, text) {
     console.log('<===== Testing - 1 =====>')
-    currentResult.textContent = result;
-    calculationResult.text = text;
+    calculatedResult.textContent = result;
+    calculationResult.textContent = text;
     console.log('<===== Testing - 2 =====>')
 }
 
